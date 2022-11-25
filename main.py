9b8962a6283d4b66efcd3ac2149a8c40a3cf1f4a
antoinergr <antoine.roger14@gmail.com>
2022-11-25 21:59:30 +0100

    change design and error handling

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import streamlit as st
 from Request.Imdb_Request import ImdbRequest
+from Request.Response import Response
 
 
-st.markdown("# **Welcome to** Do I Watch this Movie ?")
-st.write("## **THE** reference when you want to know more about a movie!")
+
+st.markdown("# **Do I Watch this Movie ?**")
+st.markdown("## **THE** reference when you want to know more about a movie!")
+
 
 title = st.text_input(
     label="Movie Name",
@@ -11,24 +14,27 @@ title = st.text_input(
     label_visibility="hidden",
 )
 
-my_movie_info = ImdbRequest.get_movie_info(title)
+with st.spinner('Nous recherchons votre film, ne bougez pas...'):
+  
+    if len(title) > 0 and not title.isspace():
+        my_movie_info = ImdbRequest.get_movie_info(title)
 
-if my_movie_info != 'HTTPError':
+        if my_movie_info != 'HTTPError':
 
-    my_movie_id = ImdbRequest.get_movie_id(my_movie_info)
+            my_movie_id = ImdbRequest.get_movie_id(my_movie_info)
 
-    if my_movie_id not in ['HTTPError','Unavailable']:
-        st.image(my_movie_id.get_poster())
-        st.markdown('## Types')
-        st.markdown('### ' + my_movie_id.get_genres())
-        st.markdown('## Plot')
-        st.markdown('### ' + my_movie_id.get_plot())
+            if  isinstance(my_movie_id,Response):
+                st.image(my_movie_id.get_poster())
+                st.markdown('## Types')
+                st.markdown('### ' + my_movie_id.get_genres())
+                st.markdown('## Plot')
+                st.markdown('### ' + my_movie_id.get_plot())
 
-        st.markdown("## Rating ")
-        st.markdown("### " + str(my_movie_id.get_rating()) + "/10")
+                st.markdown("## Rating ")
+                st.markdown("### " + str(my_movie_id.get_rating()) + "/10")
 
-        st.markdown("## Top Actors")
-        actors, images = my_movie_id.get_top_actors()
-        st.image(images, width=230, caption=actors)
-    else:
-        st.markdown("## Enter the movie name or check the name you just entered")
+                st.markdown("## Top Actors")
+                actors, images = my_movie_id.get_top_actors()
+                st.image(images, width=230, caption=actors)
+            else :
+                st.error("Oups we didn't find your movie")
